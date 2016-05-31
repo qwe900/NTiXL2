12c9be3805ce6669c83e9320fbf9c7a290451275
Enzo SR <enzo.scossa.romano@gmail.com>
2016-05-31 17:08:23 +0200

    added ykush

--- examples/serial1.py
+++ examples/serial1.py
@@ -1,5 +1,6 @@
 from serial import Serial
 from serial.tools import list_ports
+
 import sys
 
 reset = "*RST"
@@ -9,14 +10,24 @@ stop = "INIT STOP"
 mes = "MEAS:SLM:123? LAS"
 
 
-def find_port():
-    comlist = list(list_ports.comports())
-    if len(comlist):
-        XL2 = [(s1,s2,s3) for s1,s2,s3 in comlist if '1A2B:0004' in s3 ]
-    if len(XL2):
-        return XL2[0]
-
-
+def find_xl2( linux = False, filter= "XL2"):
+    comports = list(list_ports.comports())
+    attr = ['device', 'description','pid','vid','serial_number', 'product']
+    devices = []
+    for p in comports:
+        if linux:
+            attribute = {a : getattr(p,a) for a in attr}
+            q = any([filter in v for k,v in attribute.items()])
+            print('Device: ', attribute['description'], ', filter: ', q)
+            if q:
+                devices.append(attribute)
+        else:
+            attribute = p
+            q = any([(filter in v) for v in p if type(v)=='str'])
+            print('Device: ', attribute[1], ', filter: ', q)
+            if q:
+                devices.append(attribute)
+    return devices
 
 
 def command(conn,c):
@@ -57,12 +68,15 @@ def mount_status():
     out =subprocess.check_output(["sudo","blkid","-o", "list", "-w", "/dev/null"])
     print(out.decode('UTF-8'))
 
-def reset_USB():
+def ykush(command = '-d', port = 3):
+    """
+    run ykush program
+    :param command: '-u or -d'
+    :param port: 1, 2, 3 or a
+    :return: None
+    """
     import subprocess
-    subprocess.Popen(["sudo","umount", mountDir])
+    subprocess.Popen(["sudo", "~/XL2/YKUSH_V+.4.1/ykush", command, str(port)])
 
-XL2 = find_port()
-
-if XL2 is not None:
-    PORT = XL2[0]
-    conn = Serial(PORT, 9600,timeout=1)
+for conn in find_xl2():
+    conn = Serial(conn['device'], 9600,timeout=1)
